0af1fdaa7469b64e4089f51293c123a55037962c
danicaroo940 <danicaroo94@gmail.com>
2023-02-27 22:52:21 +0100

    fix error Profile.jsx

--- vite-project/src/pages/Profile.jsx
+++ vite-project/src/pages/Profile.jsx
@@ -9,7 +9,6 @@ import './Profile.scss';
 
 export const Profile = () => {
     const navigate = useNavigate()
-    const [tokenParse, setTokenParse] = useState({})
     const [userData, setUserData] = useState({})
     const token = JSON.parse(localStorage.getItem('token'))
     const axiosConfig = {
@@ -17,36 +16,24 @@ export const Profile = () => {
             'Authorization': token
         }
     }
-    function parseJwt (token) {
-        const base64Url = token.split('.')[1];
-        const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-        const jsonPayload = decodeURIComponent(window.atob(base64).split('').map(function(c) {
-            return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
-        }).join(''));
-        return JSON.parse(jsonPayload);
-    }
         useEffect(() => {
-            if(!token) {
-                navigate('/login')
-            }
-        
-            const parseToken = parseJwt(token)
-            setTokenParse(parseToken)
+            
 
         },[])
         
         useEffect(() => {
-            console.log(token)
-            const userName = tokenParse.user
-                 const getUser = async () => {
-                     const res = await axios.get (`https://backend-ecommerce-production-ce12.up.railway.app/users/id/${userName}`, axiosConfig)
-                     if (res.data) {
-                         const { username, email, role } = res.data
-                        setUserData({username, email, role})
-                     }
-                 }
-                 getUser()
-        },[tokenParse]) 
+            if(!token) {
+                navigate('/login')
+            }
+            const getUser = async () => {
+                const res = await axios.get (`https://backend-ecommerce-production-ce12.up.railway.app/users/id/`, axiosConfig)
+                if (res.data) {
+                    const { username, email, role } = res.data
+                setUserData({username, email, role})
+                }
+            }
+            getUser()
+        },[]) 
 
     return (
         <div className="profile-container">
