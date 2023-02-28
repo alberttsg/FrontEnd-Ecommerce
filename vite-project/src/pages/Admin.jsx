a28344f6bbd683361f12a9d778358a4d5f3aadb9
Alberto <albertsg.86@gmail.com>
2023-02-28 18:07:56 +0100

    crud products

--- vite-project/src/pages/Admin.jsx
+++ vite-project/src/pages/Admin.jsx
@@ -1,7 +1,10 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import axios from 'axios'
+import './Admin.scss'
 
 export const Admin = () => {
+
+  const [ products, setProducts ] = useState([])
 
   const getProducts = async () => {
 
@@ -15,6 +18,7 @@ export const Admin = () => {
   try{
     const res = await axios.get (`https://backend-ecommerce-production-ce12.up.railway.app/products/all`, axiosConfig)
       console.log(res.data)
+      setProducts([...res.data])
 
   } catch(error){
     console.log(error)
@@ -26,8 +30,19 @@ export const Admin = () => {
   },[])
 
   return (
-    <div>
-
+    <div id='bodyProducts'>
+    <div>Crear Producto</div>
+      {
+        products.map((e, index)=>(
+          <div id='allProducts' key={`products${index}`}>
+            <div>Brand: {e.brand}</div>
+            <div>Name: {e.name}</div>
+            <div>Category: {e.category}</div>
+            <img src={e.image}/>
+            <div>Price: {e.price}€</div>
+          </div>
+        ))
+      }
     </div>
   )
 }
