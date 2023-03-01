bcfbbabdd71ef31f001cf11852bfdae49a1faffb
danicaroo940 <danicaroo94@gmail.com>
2023-03-01 16:05:30 +0100

    Añadimos infinite scroll en la home page y modal de productos

--- vite-project/src/componentes/Products/Products.jsx
+++ vite-project/src/componentes/Products/Products.jsx
@@ -2,28 +2,37 @@ import React from "react";
 import './Products.scss';
 import { useEffect, useState } from "react";
 import axios from 'axios'
-import { ShoppingCartOutlined, InfoCircleOutlined ,SettingOutlined } from '@ant-design/icons';
-import { Card } from 'antd';
-import { Button, Modal } from 'antd';
+import { ShoppingCartOutlined, InfoCircleOutlined} from '@ant-design/icons';
+import { Card, Modal } from 'antd';
+
 
 export function Products() {
     const { Meta } = Card;
     const [products, setProducts] = useState([]);
     const [isModalOpen, setIsModalOpen] = useState(false);
-    const [page, setPage] = useState(0)
-    const productsPerPage = 45
-    const maxPage = Math.ceil(products.length / productsPerPage) 
-    const offSet = page * productsPerPage
-    const currentPageProducts = products.slice(offSet, offSet + productsPerPage)
-    const showModal = () => {
-        setIsModalOpen(true);
-      };
-      const handleOk = () => {
-        setIsModalOpen(false);
-      };
-      const handleCancel = () => {
-        setIsModalOpen(false);
-      };
+    const [productsPerPage, setProductsPerPage] = useState(10)
+    const [modalProduct, setModalProduct] = useState([]) 
+   
+    const currentPageProducts = products.slice(0, productsPerPage)
+   
+    const showModal = (product) => {
+      setIsModalOpen(true);
+      setModalProduct(product)
+    };
+    const handleOk = () => {
+      setIsModalOpen(false);
+    };
+    const handleCancel = () => {
+      setIsModalOpen(false);
+    };  
+    const handleScroll = () => {
+      if (window.innerHeight + document.documentElement.scrollTop +1 >= document.documentElement.scrollHeight){
+        setProductsPerPage((prev)=> prev +10)
+      }
+    }
+    useEffect(()=>{
+        window.addEventListener('scroll',handleScroll)
+    },[])
 
 
 
@@ -32,21 +41,19 @@ export function Products() {
             const res = await axios.get ('https://backend-ecommerce-production-ce12.up.railway.app/products/all')
             const data = res.data
             setProducts(data)
+           
         }   
-        getProducts()
-        
-        
+      getProducts()  
     }, [])
-    useEffect(()=>{
-        console.log(products)
-    },[products])
+  
 
-  return (
-    <div className="container">
+  return (<>
+    <div className="container" >
+    <div className="container-products">
         {currentPageProducts &&
-        
         currentPageProducts.map(product => {
-            return(<>
+          
+            return(<div key={product._id}>
                 <Card 
                     style={{
                     width: 300,
@@ -60,33 +67,31 @@ export function Products() {
                     />
                     }
                     actions={[
-                    <InfoCircleOutlined key="info"  onClick={showModal}/>,
-                    
+                    <InfoCircleOutlined key="info" id={product._id}  onClick={()=>{showModal(product)}} className=''/>,
                     <ShoppingCartOutlined key="cart" />,
                     ]}
                     >
                     <Meta
-                   
-                    title={product.name}
+                    title={product.name} 
                     description={product.price + '€'}
                     />
                     </Card>
-                     <Modal title="Basic Modal" mask={false} open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
-                     <p>Some contents...</p>
-                     <p>Some contents...</p>
-                     <p>Some contents...</p>
-                   </Modal>
-                   </>
-
-
+                   </div>
             )
-
-
         })
-        
-        }
+            } 
+                    
+                     <Modal mask={false} open={isModalOpen} onOk={handleOk} onCancel={handleCancel} className='modal'>
+                     <h1>{modalProduct.name}.</h1>
+                     <img src={modalProduct.image} alt={modalProduct.name} />
+                     <p>{modalProduct.brand}</p>
 
+                     <p className="price">Price: {modalProduct.price} €</p>
+                    </Modal>
        
     </div>
+        
+    </div>
+        </>
   )
   }
