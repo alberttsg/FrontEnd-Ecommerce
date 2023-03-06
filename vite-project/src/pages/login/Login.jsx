4051db020d7120bd1c41850b6713758af66c5d0e
laumd23 <laumd.23@gmail.com>
2023-03-06 18:07:51 +0100

    making changes in login

--- vite-project/src/pages/login/Login.jsx
+++ vite-project/src/pages/login/Login.jsx
@@ -77,16 +77,6 @@ const Login = () => {
           </Form.Item>
         </div>
         <div className="forms-container">
-          <Form.Item>
-            <Form.Item name="remember" valuePropName="checked" noStyle>
-              <Checkbox>Remember me</Checkbox>
-            </Form.Item>
-            <a className="login-form-forgot" href="">
-              Forgot password
-            </a>
-          </Form.Item>
-        </div>
-        <div className="forms-container">
           <Form.Item
             // wrapperCol={{
             //   offset: 8,
